8c403aceb2268246187521e68209bfc7f30a45ad
taro.kirameki <taro.kirameki@techacademy.jp>
2021-04-15 07:34:29 +0000

    add Status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -43,6 +43,6 @@ class TasksController < ApplicationController
     
     private
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content,:Status)
     end 
 end
