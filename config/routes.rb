b7203118e76954c66d83a5e03ff9760aac2d82ba
taro.kirameki <taro.kirameki@techacademy.jp>
2021-04-16 15:12:00 +0000

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
     
   
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
     root to: 'tasks#index'
+   get 'signup',to:'users#new'
+   get 'users',to: 'users#create'
+   get 'login',to:'sessions#new'
+   post 'login',to:'sessions#create'
+   delete 'logout',to:'sessions#destroy'
    
   resources :tasks
 end
