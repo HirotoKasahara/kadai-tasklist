5012ff2372f62427ac25a54b2921a97626618d8f
taro.kirameki <taro.kirameki@techacademy.jp>
2021-04-18 03:18:48 +0000

    kadai-task-list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :set_task,only: [:show,:edit,:update,:destroy]
+    before_action :set_task?,only: [:show,:edit,:update,:destroy]
     
     def index 
         @tasks = current_user.tasks.order(id: :desc)
@@ -45,11 +45,18 @@ class TasksController < ApplicationController
     end 
     
     private
+    
+    
     def task_params
         params.require(:task).permit(:content,:status)
     end 
     
-    def set_task
-        @task = current_user.tasks.find_by(id: params[:id])
+    def set_task?
+      if @task = current_user.tasks.find_by(id: params[:id])
+          return true
+      else 
+          flash[:danger]="###貴方のタスクではありません###"
+          redirect_to root_url
     end 
-end
+end 
+end 
