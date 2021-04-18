5d8100bc53bd56c5909f4b449564e8cff6ab3e1b
taro.kirameki <taro.kirameki@techacademy.jp>
2021-04-18 13:29:48 +0000

    aaa

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,8 +52,9 @@ class TasksController < ApplicationController
     end 
     
     def set_task
-      unless @task = current_user.tasks.find_by(id: params[:id])
-             redirect_to root_url
-       end 
+     unless @task = current_user.tasks.find_by(id: params[:id])
+         redirect_to root_url
+     end  
+       
     end 
 end 
