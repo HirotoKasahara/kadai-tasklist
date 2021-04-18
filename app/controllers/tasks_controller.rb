68aaff924e51f1971c3543735330ee140c166c19
taro.kirameki <taro.kirameki@techacademy.jp>
2021-04-18 06:15:07 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :set_task?,only: [:show,:edit,:update,:destroy]
+    before_action :set_task,only: [:show,:edit,:update,:destroy]
     
     def index 
         @tasks = current_user.tasks.order(id: :desc)
@@ -51,10 +51,10 @@ class TasksController < ApplicationController
         params.require(:task).permit(:content,:status)
     end 
     
-    def set_task?
+    def set_task
       unless @task = current_user.tasks.find_by(id: params[:id])
              flash[:danger]="###貴方のタスクではありません###"
              redirect_to root_url
+       end 
     end 
-end 
 end 
